c7eb32f047feb4899c0ef85b713690f63d9d9ab1
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2025-12-28 22:04:37 +0100

    added upload dossier, wip

--- performances/serializers.py
+++ performances/serializers.py
@@ -17,9 +17,34 @@ class PerformanceSerializer(serializers.ModelSerializer):
     creation_date = serializers.DateField(allow_null=True, required=False)
     genres = serializers.ListField(child=serializers.CharField(), required=False, allow_empty=True)
     dossiers = DossierSerializer(many=True, read_only=True)
+    dossier_files = serializers.ListField(
+        child=serializers.FileField(), write_only=True, required=False, allow_empty=True
+    )
     trailer = NormalizedURLField(required=False, allow_blank=True, max_length=100)
 
     class Meta:
         model: Type[Performance] = Performance
         fields: str = "__all__"
         read_only_fields = ("id",)
+        extra_kwargs = {"dossier_files": {"write_only": True}}
+
+    def create(self, validated_data):
+        dossier_files = validated_data.pop("dossier_files", [])
+        performance = Performance.objects.create(**validated_data)
+
+        for dossier_file in dossier_files:
+            Dossier.objects.create(performance=performance, file=dossier_file)
+
+        return performance
+
+    def update(self, instance, validated_data):
+        dossier_files = validated_data.pop("dossier_files", [])
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        for dossier_file in dossier_files:
+            Dossier.objects.create(performance=instance, file=dossier_file)
+
+        return instance
