664ce518e49b2f56f8565529251f6ff5c5b65c27
ducasse@vdpresearch.de <ducassephi@hotmail.fr>
2025-12-29 21:39:47 +0100

    fixed pdf upload and edit

--- performances/serializers.py
+++ performances/serializers.py
@@ -1,9 +1,12 @@
+import logging
 from typing import Type
 
 from rest_framework import serializers
 
-from performances.models import Dossier, Performance
 from circus_agent_backend.utils import NormalizedURLField
+from performances.models import Dossier, Performance
+
+logger = logging.getLogger(__name__)
 
 
 class DossierSerializer(serializers.ModelSerializer):
@@ -21,12 +24,14 @@ class PerformanceSerializer(serializers.ModelSerializer):
         child=serializers.FileField(), write_only=True, required=False, allow_empty=True
     )
     trailer = NormalizedURLField(required=False, allow_blank=True, max_length=100)
+    dossier_ids = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=False, allow_empty=True
+    )
 
     class Meta:
         model: Type[Performance] = Performance
         fields: str = "__all__"
         read_only_fields = ("id",)
-        extra_kwargs = {"dossier_files": {"write_only": True}}
 
     def create(self, validated_data):
         dossier_files = validated_data.pop("dossier_files", [])
@@ -34,15 +39,20 @@ class PerformanceSerializer(serializers.ModelSerializer):
 
         for dossier_file in dossier_files:
             Dossier.objects.create(performance=performance, file=dossier_file)
+            logger.info("Dossier created successfully")
 
         return performance
 
     def update(self, instance, validated_data):
         dossier_files = validated_data.pop("dossier_files", [])
-
+        dossier_ids = validated_data.pop("dossier_ids", None)
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
+
+        # If dossier_ids is provided, delete dossiers not in the list
+        if dossier_ids is not None:
+            instance.dossiers.exclude(id__in=dossier_ids).delete()
 
         for dossier_file in dossier_files:
             Dossier.objects.create(performance=instance, file=dossier_file)
