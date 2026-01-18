3e915e25aa54266cd810d468cc2467b65d2fb875
philippeducasse <ducassephi@hotmail.fr>
2026-01-18 21:42:05 +0100

    added reminders to profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include, URLPattern
+from django.urls import path, include, URLPattern, URLResolver
 from rest_framework.routers import DefaultRouter
-from profiles.views import ProfileViewSet
-from typing import List
+from profiles.views import ProfileViewSet, ReminderViewSet
+from typing import List, Union
 
 router: DefaultRouter = DefaultRouter()
 router.register(r"", ProfileViewSet, basename="profile")
-urlpatterns: List[URLPattern] = [
+router.register(r"me/reminders", ReminderViewSet, basename="reminder")
+urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path("", include(router.urls)),
 ]
