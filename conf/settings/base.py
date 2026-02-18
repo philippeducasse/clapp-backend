da96698be25f698373d49d3622133831254c356d
philippeducasse <ducassephi@hotmail.fr>
2026-02-18 16:27:12 +0100

    fix: (logging): removed healthcehck from logging

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-# import logging
+import logging
 import os
 from pathlib import Path
 
@@ -124,13 +124,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email configuration
 
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+APP_EMAIL = os.getenv("APP_EMAIL")
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 # overides default django user model
 AUTH_USER_MODEL = "profiles.Profile"
@@ -147,9 +147,9 @@ REST_FRAMEWORK = {
 }
 
 
-# class HealthcheckFilter(logging.Filter):
-#     def filter(self, record):
-#         return "/health/" not in record.getMessage()
+class HealthcheckFilter(logging.Filter):
+    def filter(self, record):
+        return "/health/" not in record.getMessage()
 
 
 LOGGING = {
@@ -161,16 +161,16 @@ LOGGING = {
             "style": "{",
         },
     },
-    # "filters": {
-    #     "healthcheck": {
-    #         "()": HealthcheckFilter,
-    #     },
-    # },
+    "filters": {
+        "healthcheck": {
+            "()": HealthcheckFilter,
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
             "formatter": "verbose",
-            # "filters": ["healthcheck"],
+            "filters": ["healthcheck"],
         },
     },
     "root": {
