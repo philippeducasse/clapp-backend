f8cf0e67d3fc720ca85d1b36b7d69a47b25c1d77
philippeducasse <ducassephi@hotmail.fr>
2026-02-18 14:33:08 +0100

    wip: testing Proxy ehader setting

--- conf/settings/prod.py
+++ conf/settings/prod.py
@@ -45,4 +45,4 @@ CELERY_TIMEZONE = os.getenv("CELERY_TIMEZONE", "UTC")
 SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True").lower() == "true"
 SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", "True").lower() == "true"
 CSRF_COOKIE_SECURE = os.getenv("CSRF_COOKIE_SECURE", "True").lower() == "true"
-# SECURE_PROXY_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
